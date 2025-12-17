77282fce695116610664aee3debc24f4f7be0fbe
jollyjoee <pisk.julijo@gmail.com>
2025-12-17 10:34:32 +0800

    Cards again

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,17 @@ function App() {
   return (
     <div>
       <Header />
-      <main className='pt-28 px-10 pb-10 flex gap-5 flex-wrap'>
-        <Card title="Ulticlick" />
-        <Card title="Modrinth projects" />
-        <Card title="MinMacro" />
-        <Card title="Foodify" />
-        <Card title="FiveM Scripts" />
-      </main>
+      <div className='pt-28 px-10 pb-5'>
+        <div>
+          <Card title="Ulticlick" className="grow min-h-120" />
+          <Card title="MinMacro" className="grow mt-5 min-h-120" />
+        </div>
+        <div className='flex gap-5 md:gap-5 flex-wrap mt-5'>
+          <Card title="Modrinth projects" className="grow" />
+          <Card title="FiveM Scripts" className="grow" />
+          <Card title="Foodify" className="grow" />
+        </div>
+      </div>
     </div>
   );
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,9 +1,8 @@
-function Card({ title }) {
+function Card({ title, className }) {
   return (
-    <div className="mx-auto my-auto flex h-80 w-64 flex-col justify-center bg-blue-400 p-2 mb-10">
-      <div className="mx-auto my-auto flex h-full w-full flex-col rounded-lg bg-black p-4" />
-      <div className="text-center text-white">{title}</div>
-    </div>
+    <div className={`
+        mx-auto my-auto flex h-80 min-w-64 flex-col items-center justify-center bg-[#3b8ebe] p-4 rounded-xl shadow-2xl ${className}
+        `}>{title}</div>
   );
 }
 
