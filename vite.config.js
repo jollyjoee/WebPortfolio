6327d2a613ed66f6418add3058181534575ec076
JollyJoe <88883844+jollyjoee@users.noreply.github.com>
2025-12-18 16:00:27 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: "es2018",
+    // This supports browsers from ~2015 onwards, covering older iPhones
+    target: 'es2015', 
+    outDir: 'dist',
   },
-});
+})
